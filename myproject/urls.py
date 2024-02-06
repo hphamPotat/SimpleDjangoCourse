c3b7ec1c53c953c7d1ce0f4d9bca98cee0ef6095
Huy Pham <phamduchuy2000@gmail.com>
2024-02-05 22:12:14 -0500

    Added index link

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('onlinecourse.urls')),
     path('onlinecourse/', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
